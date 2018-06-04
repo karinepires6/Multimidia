307809954c38125f3516ad3f0e942c00f113b3c9
Karine <pires.karine6@gmail.com>
2018-06-04 14:02:23 -0300

    Correcao codigo de extracao de pele

--- ExtracaoPele.py
+++ ExtracaoPele.py
@@ -12,18 +12,18 @@ largura = img.shape[1]
 altura = img.shape[0]
 
 #Define uma matriz com zeros com as dimensões da imagem original
-matrizf = np.zeros((largura, altura))
+blank_image = np.zeros((altura,largura,1), np.uint8)
 
 #Percorre os pixels das matrizes RGB
-for coluna in range(0, largura):
-  for linha in range(0, altura):
+for linha in range(0, largura):
+  for coluna in range(0, altura):  
     #Verifica se o pixel representa um pixel de pele
-    if (R[linha, coluna] > 120 and R[linha, coluna] < 255) and (G[linha, coluna] > 90 and G[linha, coluna] < 250) and (R[linha, coluna] > 70 and R[linha, coluna] < 218) and (R[linha, coluna] > G[linha, coluna]) and (G[linha, coluna] > B[linha, coluna]):
-      matrizf[coluna, linha] = 1
+    if (R[coluna, linha] > 120 and R[coluna, linha] < 255) and (G[coluna, linha] > 90 and G[coluna, linha] < 250) and (B[coluna, linha] > 70 and B[coluna, linha] < 218) and (R[coluna, linha] > G[coluna, linha]) and (G[coluna, linha] > B[coluna, linha]):
+      #lin.append(0)
+      blank_image[coluna, linha] = 0
     else:
-      matrizf[coluna, linha] = 0
+      blank_image[coluna, linha] = 255
 
 #Exibe a suposta matriz com os pixels de pele da matriz original
-cv2.imshow("Matriz final", matrizf)
-
+cv2.imshow("Matriz final", blank_image)
 cv2.waitKey(0)
